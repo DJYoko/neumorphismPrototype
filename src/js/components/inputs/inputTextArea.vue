3289246e2358be8361cb7644e2a3d0fe679b2dea
DJYoko <yokochan.recurrent@gmail.com>
2020-08-10 22:50:17 +0900

    add input textarea components

---
+++ src/js/components/inputs/inputTextArea.vue
@@ -0,0 +1,96 @@
+<template>
+  <textarea
+    class="p-inputTextArea"
+    :class="styleClasses"
+    :disabled="isDisabled"
+    v-model="form.text"
+  />
+</template>
+
+<script>
+export default {
+  name: 'inputText',
+  components: {},
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    isFocus: {
+      type: Boolean,
+      default: false,
+    },
+    isDisabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data: function() {
+    return {
+      form: {
+        text: '',
+      },
+    }
+  },
+  computed: {
+    styleClasses() {
+      const classList = []
+
+      if (this.$props.isFocus) {
+        classList.push('is-focus')
+      }
+      if (this.$props.isDisabled) {
+        classList.push('is-disabled')
+      }
+
+      return classList.join(' ')
+    },
+  },
+  created() {
+    this.form.text = this.$props.text
+  },
+  methods: {},
+}
+</script>
+
+<style lang="scss">
+$baseSize: 4px;
+.p-inputTextArea {
+  appearance: none;
+  border: none;
+  align-items: center;
+  border: 1px solid rgba(0, 0, 0, 0);
+  background-color: #e6e7ee;
+  // prettier-ignore
+  box-shadow:
+    calc(0px) calc(0px) calc(0px) 0 rgba(0, 0, 0, 0.25),
+    calc(0px) calc(0px) calc(0px) 0 rgba(255, 255, 255, 0.5),
+    inset calc(#{$baseSize} * 3) calc(#{$baseSize} * 3) calc(#{$baseSize} * 4) 0 rgba(0, 0, 0, 0.25),
+    inset calc(#{$baseSize} * -2) calc(#{$baseSize} * -2) calc(#{$baseSize} * 3) 0 rgba(255, 255, 255, 0.5)
+  ;
+  border-radius: 20px;
+  justify-content: center;
+  padding: 16px 32px;
+  transition: all 0.2s ease-out;
+  border-bottom: 1px solid rgba(255, 255, 255, 1);
+  border-right: 1px solid rgba(255, 255, 255, 1);
+  border-top: 1px solid rgba(0, 0, 0, 0.25);
+  border-left: 1px solid rgba(0, 0, 0, 0.25);
+
+  &:focus,
+  &.is-focus {
+    outline: none;
+    // prettier-ignore
+  }
+
+  &:disabled,
+  &.is-disabled {
+    cursor: not-allowed;
+    box-shadow: none;
+    // border-top: 1px solid rgba(255, 255, 255, 0.5);
+    // border-left: 1px solid rgba(255, 255, 255, 0.5);
+    // border-right: 1px solid rgba(0, 0, 0, 0.25);
+    // border-bottom: 1px solid rgba(0, 0, 0, 0.25);
+  }
+}
+</style>
--- src/js/components/inputs/inputsIndex.vue
+++ src/js/components/inputs/inputsIndex.vue
@@ -1,16 +1,30 @@
 <template>
   <div class="p-inputsIndex">
     <h3 class="display-4">input text</h3>
-    <inputText class="mr-4" text="input default" />
-    <inputText class="mr-4" text="input disabled" :is-disabled="true" />
+    <inputText class="mr-4 mb-2 w-25" text="input default" />
+    <inputText
+      class="mr-4 mb-2 w-25"
+      text="input disabled"
+      :is-disabled="true"
+    />
+    <div class="mb-2">
+      <h3 class="display-4">input textarea</h3>
+      <inputTextArea class="mr-4 w-25" text="textarea default" />
+      <inputTextArea
+        class="mr-4 w-25"
+        text="textarea disabled"
+        :is-disabled="true"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import inputText from './inputText'
+import inputTextArea from './inputTextArea'
 export default {
   name: 'inputsIndex',
-  components: { inputText },
+  components: { inputText, inputTextArea },
   props: {},
   data: function() {
     return {}
