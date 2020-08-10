4e3852abf77992c152d7b50cb16400ac78e38327
DJYoko <yokochan.recurrent@gmail.com>
2020-08-10 22:28:18 +0900

    add input text components

--- src/js/components/index.vue
+++ src/js/components/index.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="p-index">
-    <buttonsIndex />
+    <buttonsIndex class="mb-5" />
+    <inputsIndex class="mb-5" />
   </div>
 </template>
 
 <script>
 import buttonsIndex from './buttons/buttonsIndex'
+import inputsIndex from './inputs/inputsIndex'
 export default {
   name: 'index',
-  components: { buttonsIndex },
+  components: { buttonsIndex, inputsIndex },
   props: {},
   data: function() {
     return {}
