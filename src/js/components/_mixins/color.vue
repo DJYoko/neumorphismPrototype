418f5aaaf60df04ec81570fa7c72552ec196319e
DJYoko <yokochan.recurrent@gmail.com>
2020-08-16 23:54:35 +0900

    add Mixins for color styles.

---
+++ src/js/components/_mixins/color.vue
@@ -0,0 +1,47 @@
+<script>
+export default {
+  name: 'color',
+  props: {
+    bgColor: {
+      type: String,
+      default: '',
+    },
+    textColor: {
+      type: String,
+      default: '',
+    },
+    isFocus: {
+      type: Boolean,
+      default: false,
+    },
+    isDisabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data: function() {
+    return {}
+  },
+  computed: {
+    colorStyles() {
+      return {
+        backgroundColor: this.$props.bgColor,
+        color: this.$props.textColor,
+      }
+    },
+    styleClasses() {
+      const classList = []
+
+      if (this.$props.isFocus) {
+        classList.push('is-focus')
+      }
+      if (this.$props.isDisabled) {
+        classList.push('is-disabled')
+      }
+
+      return classList.join(' ')
+    },
+  },
+  methods: {},
+}
+</script>
